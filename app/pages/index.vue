d6fabb2b8b1a16bfba23ce5531394cfd937b6b66
H.G <hawigemechis1@gmail.com>
2026-02-09 18:10:41 +0300

    new Nuxt app

---
+++ app/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+<h2>Home</h2>
+<p>HelloSPEC Devs!</p>
+<NuxtLink to="/about">About Page</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom:20px;
+    font-size:36px;
+}
+p{
+    margin:20px 0;
+}
+</style>
