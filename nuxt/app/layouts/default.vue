4ce62ad3a5f69fa81e13ca5ada2f0dd9bc19739c
H.G <hawigemechis1@gmail.com>
2026-02-19 23:37:00 +0300

    Import nuxt branch into nuxt folder

---
+++ nuxt/app/layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <NuxtLink to="/" class="nav-link">Home</NuxtLink>
+      <NuxtLink to="/about" class="nav-link">About</NuxtLink>
+      <NuxtLink to="/products" class="nav-link">Products</NuxtLink>
+
+    </nav>
+
+    <slot />
+  </div>
+</template>
+
+
+<style scoped>
+.navbar {
+  display: flex;              
+  justify-content: flex-end;  
+  align-items: center;       
+  padding: 1rem;
+  background-color: #e3c0da;
+  border-bottom: 1px solid #ccc;
+}
+
+.nav-link {
+  margin-left: 1rem;
+  font-weight: bold;
+  color: rgb(91, 35, 101);
+  text-decoration: underline;
+}
+
+
+
+</style>
---
+++ nuxt/app/pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="about-container">
+    <h1>About Us</h1>
+    <p>
+      Welcome! We’re passionate about delivering great products and experiences.
+      Our mission is to provide quality and value to all our customers.
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.about-container {
+  max-width: 700px;
+  margin: 4rem auto;
+  padding: 0 1rem;
+  text-align: center;
+  font-family: system-ui, sans-serif;
+  color: #333;
+}
+
+.about-container h1 {
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+}
+
+.about-container p {
+  font-size: 1.125rem;
+  line-height: 1.6;
+}
+</style>
